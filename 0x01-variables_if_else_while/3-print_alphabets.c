5cfefd784416c3000c7bd28d74bcdfeb2bf8b991
kakamishaeli <kakamishaeli@gmail.com>
2023-11-20 06:53:52 -0800

    lower and upper alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,13 +7,19 @@
  */
 int main(void)
 {
-	char a;
+	char a, b;
 
 	a = 'a';
+	b = 'A';
 	while (a <= 'z')
 	{
 		putchar(a);
 		a++;
+	}
+	while (b <= 'Z')
+	{
+		putchar(b);
+		b++;
 	}
 	putchar('\n');
 	return (0);
