be4a4117d1ab761b0187ca554116a461e6d21434
kakamishaeli <kakamishaeli@gmail.com>
2023-11-20 07:34:48 -0800

    my base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - single digites base ten
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char a = '0';
+
+	while ( a <= '9')
+	{
+		putchar(a);
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
