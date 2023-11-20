e232999b4f1d6f8cb0cbbdf38bb3f670676db1bf
kakamishaeli <kakamishaeli@gmail.com>
2023-11-20 07:49:56 -0800

    base 10 using int

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int a = '0';
+
+	while (a <= '9')
+	{
+		putchar(a);
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
