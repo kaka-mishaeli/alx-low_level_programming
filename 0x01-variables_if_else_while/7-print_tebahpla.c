f11a0ec400224e499d0c4821f0a81ba7f38c9f57
kakamishaeli <kakamishaeli@gmail.com>
2023-11-20 07:56:30 -0800

    reverse alphabets

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main -entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	char a = 'z';
+
+	while (a >= 'a')
+	{
+		putchar(a);
+		a--;
+	}
+	putchar('\n');
+	return (0);
+}
