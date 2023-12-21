51e428a0267829dbcce3400ab64d84f1cbbd81aa
kakamishaeli <kakamishaeli@gmail.com>
2023-12-21 05:59:14 -0800

    my task 1

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -8,24 +8,18 @@
  * Return: The number of elements in the dlistint_t list.
  */
 size_t dlistint_len(const dlistint_t *h)
+
 {
-    /*Variable to store the number of elements in the list.*/
-    size_t nodes = 0;
+	size_t nodes = 0;
+
+	while (h)
+	{
+	nodes++;
+	/*Move to the next element.*/
+	h = h->next;
+	}
 
 
-    while (h)
-    {
-        /*Increment the element count.*/
-        nodes++;
-
-
-
-
-        /*Move to the next element.*/
-        h = h->next;
-    }
-
-
-    /*Return the total number of elements in the list.*/
-    return (nodes);
+	/*Return the total number of elements in the list.*/
+	return (nodes);
 }
